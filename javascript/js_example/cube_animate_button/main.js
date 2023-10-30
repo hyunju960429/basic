cbf59af991bdefaf3481dbbc7731ed4369ca1ea6
hyunju960429 <145514544+hyunju960429@users.noreply.github.com>
2023-10-30 17:32:15 +0900

    Delete javascript directory

--- javascript/js_example/cube_animate_button/main.js
+++
@@ -1,9 +0,0 @@
-let rotationX=0;
-let rotationY=0;
-
-const cube = document.querySelector('.box-area');
-
-function rotateXAxis(n){
-    rotationX = rotationX + (90*n);
-    console.log(rotationX);
-}
